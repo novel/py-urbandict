b957b5eaf382a0d987697736b265cce231095ef2
Roman Bogorodskiy <bogorodskiy@gmail.com>
2011-02-27 12:06:42 +0300

    Switch to python3.

--- urbandicli.py
+++ urbandicli.py
@@ -1,29 +1,32 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.2
 #
 # Simple interface to urbandictionary.com
 #
 # Author: Roman Bogorodskiy <bogorodskiy@gmail.com>
 
-import urllib2
+import urllib.request, urllib.error, urllib.parse
 import sys
-from HTMLParser import HTMLParser
-from urllib import quote as urlquote
+import textwrap
+from html.parser import HTMLParser
+from urllib.parse import quote as urlquote
 
 class UrbanDictParser(HTMLParser):
     inside_index_item = False
     inside_word_section = False
     inside_def_section = False
     inside_example_section = False
-    current_index = None
-    translations = {}
+#    current_index = 0
+    translations = []
 
     def handle_starttag(self, tag, attrs):
         if tag == "td":
-            if len(attrs) > 0:
-                if attrs[0][0] == 'class' and attrs[0][1] == 'index':
+            attrs_dict = dict(attrs)
+
+            if 'class' in attrs_dict:
+                if attrs_dict['class'] == 'index':
                     self.inside_index_item = True
-                elif attrs[0][0] == 'class' and attrs[0][1] == 'word':
-                        self.inside_word_section = True
+                elif attrs_dict['class'] == 'word':
+                    self.inside_word_section = True
         elif tag == "div":
             if len(attrs) > 0:
                 if attrs[0][0] == 'class' and attrs[0][1] == 'definition':
@@ -40,24 +43,26 @@ class UrbanDictParser(HTMLParser):
 
     def handle_data(self, data):
         if self.inside_index_item is True:
-            self.current_index = data.strip()[:-1]
-            self.translations[self.current_index] = {}
-            self.translations[self.current_index]['def'] = ''
-            self.translations[self.current_index]['example'] = ''
+ #           self.current_index += 1
+            self.translations.append({})
+#            self.translations[:-1] = {}
+#            print(self.translations[-1])
+            self.translations[-1]['def'] = ''
+            self.translations[-1]['example'] = ''
             self.inside_index_item = False
         elif self.inside_word_section is True:
-            self.translations[self.current_index]['word'] = data.strip()
+            self.translations[-1]['word'] = data.strip()
             self.inside_word_section = False
         elif self.inside_def_section is True:
-            self.translations[self.current_index]['def'] += data
+            self.translations[-1]['def'] += data.replace('\r', '\n')
         elif self.inside_example_section is True:
-            self.translations[self.current_index]['example'] += data
+            self.translations[-1]['example'] += data.replace('\r', '\n')
 
     def getTranslations(self):
         return self.translations
 
 def usage():
-    print "Usage: %s <term>\n" % sys.argv[0]
+    print("Usage: %s <term>\n" % sys.argv[0])
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -66,20 +71,18 @@ if __name__ == "__main__":
 
     term = urlquote(sys.argv[1])
 
-    f = urllib2.urlopen("http://www.urbandictionary.com/define.php?term=%s" % term)
-    data = f.read()
+    f = urllib.request.urlopen("http://www.urbandictionary.com/define.php?term=%s" % term)
+    data = f.read().decode('utf-8')
 
     urbanDictParser = UrbanDictParser()
     urbanDictParser.feed(data)
     translations = urbanDictParser.getTranslations()
 
-    # pretty print them
-    indexen = translations.keys()
-    indexen.sort()
+    for index in range(len(translations)):
+        print("%s. %s" % (index + 1, translations[index]['word']))
+        print('\n'.join(textwrap.wrap(translations[index]['def'])))
 
-    for index in indexen:
-        print "%s. %s" % (index, translations[index]['word'])
-        print translations[index]['def'].replace('\r', '\n')
         if translations[index]['example'] != '':
-            print "Examples:"
-            print translations[index]['example'].replace('\r', '\n')
+            print("Examples:")
+            print('\n'.join(textwrap.wrap(translations[index]['example'])))
+        print("\n")
