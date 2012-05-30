a620f53d704fe08cb7eaae90542628f3108835ea
Yuriy Taraday <yorik.sar@gmail.com>
2012-05-30 23:10:31 +0400

    Add classifiers to setup.py.

--- setup.py
+++ setup.py
@@ -4,4 +4,8 @@ setup(name='urbandict',
         version='0.1',
         py_modules=['urbandict'],
         scripts=['urbandicli'],
+        classifiers=[
+                "Programming Language :: Python :: 2",
+                "Programming Language :: Python :: 3",
+            ],
         )
