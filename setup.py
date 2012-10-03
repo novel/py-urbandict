7fceaa5ac60f450918b2e66a4bed1e1546957cba
Sumeet Agarwal <sumeet.a@gmail.com>
2012-10-03 00:10:24 -0700

    make `python setup.py test` work under Python 2 and 3

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
-from distutils.core import setup
+# Causes `python setup.py test` to not crash.
+# See https://groups.google.com/forum/#!msg/nose-users/fnJ-kAUbYHQ/_UsLN786ygcJ
+import multiprocessing
+from setuptools import setup
 
 setup(name='urbandict',
         version='0.1',
         py_modules=['urbandict'],
         scripts=['urbandicli'],
+        test_suite='nose.collector',
+        test_requires=['nose', 'mock'],
         classifiers=[
                 "Programming Language :: Python :: 2",
                 "Programming Language :: Python :: 3",
