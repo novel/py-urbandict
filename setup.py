7db0e44e993e6441286dd1809086169ea8cbf30c
Roman Bogorodskiy <bogorodskiy@gmail.com>
2014-04-01 10:30:57 +0400

    Drop test
    Testing over fixtures doesn't save us from the
    markup changes so it's almost useless.

--- setup.py
+++ setup.py
@@ -7,8 +7,6 @@ setup(name='urbandict',
         version='0.1',
         py_modules=['urbandict'],
         scripts=['urbandicli'],
-        test_suite='nose.collector',
-        test_requires=['nose', 'mock'],
         classifiers=[
                 "Programming Language :: Python :: 2",
                 "Programming Language :: Python :: 3",
