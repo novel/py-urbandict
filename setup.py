602449048f81f913b0b9a0da9330faff97631a72
Roman Bogorodskiy <bogorodskiy@gmail.com>
2019-10-20 07:48:34 +0400

    Bump version to 0.6.1 and include long description

--- setup.py
+++ setup.py
@@ -3,13 +3,20 @@
 import multiprocessing  # noqa
 from setuptools import setup
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
 setup(name='urbandict',
-      version='0.6',
+      version='0.6.1',
       py_modules=['urbandict'],
       scripts=['urbandicli'],
       author='Roman Bogorodskiy',
       author_email='bogorodskiy@gmail.com',
       description='CLI client and a library for urbandictionary.com',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/novel/py-urbandict',
       classifiers=[
                 "Environment :: Console",
